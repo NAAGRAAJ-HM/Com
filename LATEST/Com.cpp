4a279d651f1a5e57246ad3ebe906fd9bb0f0b93d
root <nagaraja.hm.0001@gmail.com>
2022-01-03 22:12:50 +0100

    Update: submodule Com

--- LATEST/Com.cpp
+++ LATEST/Com.cpp
@@ -5,3 +5,123 @@ class_Com Com;
 void class_Com::InitFunction(void){
 }
 
+void Class_Com::DeInitFunction(void){
+}
+
+void Class_Com::IpduGroupStart(void){
+}
+
+void Class_Com::IpduGroupStop(void){
+}
+
+void Class_Com::EnableReceptionDM(void){
+}
+
+void Class_Com::DisableReceptionDM(void){
+}
+
+void Class_Com::GetStatus(void){
+}
+
+void Class_Com::GetVersionInfo(void){
+}
+
+void Class_Com::SendSignal(void){
+}
+
+void Class_Com::SendDynSignal(void){
+}
+
+void Class_Com::ReceiveSignal(void){
+}
+
+void Class_Com::ReceiveDynSignal(void){
+}
+
+void Class_Com::SendSignalGroup(void){
+}
+
+void Class_Com::ReceiveSignalGroup(void){
+}
+
+void Class_Com::SendSignalGroupArray(void){
+}
+
+void Class_Com::ReceiveSignalGroupArray(void){
+}
+
+void Class_Com::InvalidateSignal(void){
+}
+
+void Class_Com::InvalidateSignalGroup(void){
+}
+
+void Class_Com::TriggerIpduSend(void){
+}
+
+void Class_Com::TriggerIpduSendWithMetaData(void){
+}
+
+void Class_Com::SwitchIpduTxMode(void){
+}
+
+void Class_Com::CbTriggerTransmit(void){
+}
+
+void Class_Com::CbRxIndication(void){
+}
+
+void Class_Com::CbTpRxIndication(void){
+}
+
+void Class_Com::CbTxConfirmation(void){
+}
+
+void Class_Com::CbTpTxConfirmation(void){
+}
+
+void Class_Com::CbStartOfReception(void){
+}
+
+void Class_Com::CbCopyRxData(void){
+}
+
+void Class_Com::CbCopyTxData(void){
+}
+
+void Class_Com::MainFunctionRx(void){
+}
+
+void Class_Com::MainFunctionTx(void){
+}
+
+void Class_Com::MainFunctionRouteSignals(void){
+}
+
+void Class_Com::CbTxAck(void){
+}
+
+void Class_Com::CbTxErr(void){
+}
+
+void Class_Com::CbTxTOut(void){
+}
+
+void Class_Com::CbRxAck(void){
+}
+
+void Class_Com::CbRxTOut(void){
+}
+
+void Class_Com::CbRxInv(void){
+}
+
+void Class_Com::CbCounterErr(void){
+}
+
+void Class_Com::CallOutRxIpdu(void){
+}
+
+void Class_Com::CallOutTxIpdu(void){
+}
+
--- LATEST/Com.h
+++ LATEST/Com.h
@@ -2,7 +2,47 @@
 
 class class_Com{
    public:
-      void InitFunction(void);
+      void InitFunction                (void);
+      void DeInitFunction              (void);
+      void IpduGroupStart              (void);
+      void IpduGroupStop               (void);
+      void EnableReceptionDM           (void);
+      void DisableReceptionDM          (void);
+      void GetStatus                   (void);
+      void GetVersionInfo              (void);
+      void SendSignal                  (void);
+      void SendDynSignal               (void);
+      void ReceiveSignal               (void);
+      void ReceiveDynSignal            (void);
+      void SendSignalGroup             (void);
+      void ReceiveSignalGroup          (void);
+      void SendSignalGroupArray        (void);
+      void ReceiveSignalGroupArray     (void);
+      void InvalidateSignal            (void);
+      void InvalidateSignalGroup       (void);
+      void TriggerIpduSend             (void);
+      void TriggerIpduSendWithMetaData (void);
+      void SwitchIpduTxMode            (void);
+      void CbTriggerTransmit           (void);
+      void CbRxIndication              (void);
+      void CbTpRxIndication            (void);
+      void CbTxConfirmation            (void);
+      void CbTpTxConfirmation          (void);
+      void CbStartOfReception          (void);
+      void CbCopyRxData                (void);
+      void CbCopyTxData                (void);
+      void MainFunctionRx              (void);
+      void MainFunctionTx              (void);
+      void MainFunctionRouteSignals    (void);
+      void CbTxAck                     (void);
+      void CbTxErr                     (void);
+      void CbTxTOut                    (void);
+      void CbRxAck                     (void);
+      void CbRxTOut                    (void);
+      void CbRxInv                     (void);
+      void CbCounterErr                (void);
+      void CallOutRxIpdu               (void);
+      void CallOutTxIpdu               (void);
 };
 
 extern class_Com Com;
