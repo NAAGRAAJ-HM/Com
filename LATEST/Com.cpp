13986daf1e01a3724b1dc593613d215b2f765db8
root <nagaraja.hm.0001@gmail.com>
2022-01-11 09:04:36 +0100

    Update: submodule Com

--- LATEST/Com.cpp
+++ LATEST/Com.cpp
@@ -5,123 +5,123 @@ class_Com Com;
 void class_Com::InitFunction(void){
 }
 
-void Class_Com::DeInitFunction(void){
+void class_Com::DeInitFunction(void){
 }
 
-void Class_Com::IpduGroupStart(void){
+void class_Com::IpduGroupStart(void){
 }
 
-void Class_Com::IpduGroupStop(void){
+void class_Com::IpduGroupStop(void){
 }
 
-void Class_Com::EnableReceptionDM(void){
+void class_Com::EnableReceptionDM(void){
 }
 
-void Class_Com::DisableReceptionDM(void){
+void class_Com::DisableReceptionDM(void){
 }
 
-void Class_Com::GetStatus(void){
+void class_Com::GetStatus(void){
 }
 
-void Class_Com::GetVersionInfo(void){
+void class_Com::GetVersionInfo(void){
 }
 
-void Class_Com::SendSignal(void){
+void class_Com::SendSignal(void){
 }
 
-void Class_Com::SendDynSignal(void){
+void class_Com::SendDynSignal(void){
 }
 
-void Class_Com::ReceiveSignal(void){
+void class_Com::ReceiveSignal(void){
 }
 
-void Class_Com::ReceiveDynSignal(void){
+void class_Com::ReceiveDynSignal(void){
 }
 
-void Class_Com::SendSignalGroup(void){
+void class_Com::SendSignalGroup(void){
 }
 
-void Class_Com::ReceiveSignalGroup(void){
+void class_Com::ReceiveSignalGroup(void){
 }
 
-void Class_Com::SendSignalGroupArray(void){
+void class_Com::SendSignalGroupArray(void){
 }
 
-void Class_Com::ReceiveSignalGroupArray(void){
+void class_Com::ReceiveSignalGroupArray(void){
 }
 
-void Class_Com::InvalidateSignal(void){
+void class_Com::InvalidateSignal(void){
 }
 
-void Class_Com::InvalidateSignalGroup(void){
+void class_Com::InvalidateSignalGroup(void){
 }
 
-void Class_Com::TriggerIpduSend(void){
+void class_Com::TriggerIpduSend(void){
 }
 
-void Class_Com::TriggerIpduSendWithMetaData(void){
+void class_Com::TriggerIpduSendWithMetaData(void){
 }
 
-void Class_Com::SwitchIpduTxMode(void){
+void class_Com::SwitchIpduTxMode(void){
 }
 
-void Class_Com::CbTriggerTransmit(void){
+void class_Com::CbTriggerTransmit(void){
 }
 
-void Class_Com::CbRxIndication(void){
+void class_Com::CbRxIndication(void){
 }
 
-void Class_Com::CbTpRxIndication(void){
+void class_Com::CbTpRxIndication(void){
 }
 
-void Class_Com::CbTxConfirmation(void){
+void class_Com::CbTxConfirmation(void){
 }
 
-void Class_Com::CbTpTxConfirmation(void){
+void class_Com::CbTpTxConfirmation(void){
 }
 
-void Class_Com::CbStartOfReception(void){
+void class_Com::CbStartOfReception(void){
 }
 
-void Class_Com::CbCopyRxData(void){
+void class_Com::CbCopyRxData(void){
 }
 
-void Class_Com::CbCopyTxData(void){
+void class_Com::CbCopyTxData(void){
 }
 
-void Class_Com::MainFunctionRx(void){
+void class_Com::MainFunctionRx(void){
 }
 
-void Class_Com::MainFunctionTx(void){
+void class_Com::MainFunctionTx(void){
 }
 
-void Class_Com::MainFunctionRouteSignals(void){
+void class_Com::MainFunctionRouteSignals(void){
 }
 
-void Class_Com::CbTxAck(void){
+void class_Com::CbTxAck(void){
 }
 
-void Class_Com::CbTxErr(void){
+void class_Com::CbTxErr(void){
 }
 
-void Class_Com::CbTxTOut(void){
+void class_Com::CbTxTOut(void){
 }
 
-void Class_Com::CbRxAck(void){
+void class_Com::CbRxAck(void){
 }
 
-void Class_Com::CbRxTOut(void){
+void class_Com::CbRxTOut(void){
 }
 
-void Class_Com::CbRxInv(void){
+void class_Com::CbRxInv(void){
 }
 
-void Class_Com::CbCounterErr(void){
+void class_Com::CbCounterErr(void){
 }
 
-void Class_Com::CallOutRxIpdu(void){
+void class_Com::CallOutRxIpdu(void){
 }
 
-void Class_Com::CallOutTxIpdu(void){
+void class_Com::CallOutTxIpdu(void){
 }
 
