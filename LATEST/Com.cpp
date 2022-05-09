2b7fe1e85e745bb5b5aee88077be43d27ed34e6d
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:36:01 +0000

    Update: AUTOSAR basic software service module Com

--- LATEST/Com.cpp
+++ LATEST/Com.cpp
@@ -35,6 +35,13 @@ class module_Com:
       public abstract_module
    ,  public class_Com_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Com(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
