53b490e89051c5315db09b587a684f9f6bde8cd9
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-12-27 20:39:05 +0100

    Update: AUTOSAR basic software module for communication services

---
+++ main_R2023/ServiceCom.cpp
@@ -0,0 +1,330 @@
+/******************************************************************************/
+/* File   : ServiceCom.cpp                                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "ServiceCom.hpp"
+#include "infServiceCom_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define SERVICECOM_AR_RELEASE_VERSION_MAJOR                                    4
+#define SERVICECOM_AR_RELEASE_VERSION_MINOR                                    3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(SERVICECOM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible SERVICECOM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(SERVICECOM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible SERVICECOM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_ServiceCom, SERVICECOM_VAR) ServiceCom;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, SERVICECOM_CONST,       SERVICECOM_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICECOM_CONFIG_DATA, SERVICECOM_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == ServiceCom_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstServiceCom_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == ServiceCom_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == ServiceCom_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ServiceCom_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICECOM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::DeInitFunction(
+   void
+){
+#if(STD_ON == ServiceCom_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceCom_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == ServiceCom_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICECOM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::MainFunction(
+   void
+){
+#if(STD_ON == ServiceCom_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == ServiceCom_InitCheck)
+   }
+   else{
+#if(STD_ON == ServiceCom_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  SERVICECOM_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::MainFunctionRx(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::MainFunctionTx(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::IpduGroupStart(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::IpduGroupStop(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::EnableReceptionDM(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::DisableReceptionDM(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::GetStatus(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::SendSignal(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::SendDynSignal(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::ReceiveSignal(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::ReceiveDynSignal(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::SendSignalGroup(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::ReceiveSignalGroup(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::SendSignalGroupArray(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::ReceiveSignalGroupArray(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::InvalidateSignal(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::InvalidateSignalGroup(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::TriggerIpduSend(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::TriggerIpduSendWithMetaData(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::SwitchIpduTxMode(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTriggerTransmit(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::RxIndication(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTpRxIndication(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::TxConfirmation(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTpTxConfirmation(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbStartOfReception(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbCopyRxData(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbCopyTxData(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTxAck(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTxErr(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbTxTOut(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbRxAck(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbRxTOut(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbRxInv(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CbCounterErr(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CallOutRxIpdu(
+   void
+){
+}
+
+FUNC(void, SERVICECOM_CODE) module_ServiceCom::CallOutTxIpdu(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ main_R2023/ServiceCom.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : ServiceCom.hpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstServiceCom.hpp"
+#include "CfgServiceCom.hpp"
+#include "ServiceCom_core.hpp"
+#include "infServiceCom_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_ServiceCom:
+      INTERFACES_EXPORTED_SERVICECOM
+      public abstract_module
+   ,  public class_ServiceCom_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstServiceCom_Type* lptrConst = (ConstServiceCom_Type*)NULL_PTR;
+      infServicePduRClient_Up infServicePduRClient_ServiceCom;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, SERVICECOM_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, SERVICECOM_CONST,       SERVICECOM_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   SERVICECOM_CONFIG_DATA, SERVICECOM_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, SERVICECOM_CODE) DeInitFunction (void);
+      FUNC(void, SERVICECOM_CODE) MainFunction   (void);
+      SERVICECOM_CORE_FUNCTIONALITIES
+
+      FUNC(void, SERVICECOM_CODE) MainFunctionRx (void);
+      FUNC(void, SERVICECOM_CODE) MainFunctionTx (void);
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_ServiceCom, SERVICECOM_VAR) ServiceCom;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
